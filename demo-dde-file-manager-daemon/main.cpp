5ef5651bd57eebbc315b5c393976610db834402e
zhangsheng <zhangsheng@uniontech.com>
2020-09-22 14:01:28 +0800

    Title:feat, add watchdog and singleapp to other services.
    Description:
    
    RootCause:Null
    
    Solution:

--- demo-dde-file-manager-daemon/main.cpp
+++ demo-dde-file-manager-daemon/main.cpp
@@ -1,8 +1,29 @@
 #include <QCoreApplication>
+#include <QDebug>
+
+#include "singleapp.hpp"
+#include "watchdog.hpp"
+
+void business()
+{
+
+}
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    if (SingleApplication::isRunning()) {
+        qDebug() << "Process is already running";
+        return 1;
+    }
+
+    qDebug() << "main pid: " << getpid();
+    bool ret = WatchDog::start(business);
+    if (!ret) {
+        qDebug() << "Init watch dog failed";
+        return 1;
+    }
 
     return a.exec();
 }
